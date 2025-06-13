2bfbe2d047e114bad14095c1f33ac14bdff11aeb
nate254347 <nate254347@gmail.com>
2025-06-12 21:44:14 -0400

    Added Calendar Template
    Added:
    Calendar Ui panel
    Calendar month navigation buttons
    Active Date and Time Display
    Calendar manager  script

---
+++ Assets/Scripts/Calendar/CalendarManager.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CalendarManager : MonoBehaviour
+{
+    public TextMeshProUGUI monthLabel;
+    public Button prevButton;
+    public Button nextButton;
+    public GameObject dayCellPrefab;
+    public Transform dayGrid;
+
+    private DateTime currentDate;
+
+    void Start()
+    {
+        currentDate = DateTime.Now;
+        GenerateCalendar(currentDate.Year, currentDate.Month);
+
+        prevButton.onClick.AddListener(() => ChangeMonth(-1));
+        nextButton.onClick.AddListener(() => ChangeMonth(1));
+    }
+
+    void ChangeMonth(int offset)
+    {
+        currentDate = currentDate.AddMonths(offset);
+        GenerateCalendar(currentDate.Year, currentDate.Month);
+    }
+
+    void GenerateCalendar(int year, int month)
+    {
+        monthLabel.text = $"{year}\n{new DateTime(year, month, 1).ToString("MMMM")}";
+
+
+        foreach (Transform child in dayGrid)
+            Destroy(child.gameObject);
+
+        int daysInMonth = DateTime.DaysInMonth(year, month);
+        int startDay = (int)new DateTime(year, month, 1).DayOfWeek;
+
+        for (int i = 0; i < startDay; i++)
+            Instantiate(dayCellPrefab, dayGrid);
+
+        for (int day = 1; day <= daysInMonth; day++)
+        {
+            GameObject cell = Instantiate(dayCellPrefab, dayGrid);
+            TextMeshProUGUI dayText = cell.GetComponentInChildren<TextMeshProUGUI>();
+            dayText.text = day.ToString();
+
+            if (year == DateTime.Now.Year && month == DateTime.Now.Month && day == DateTime.Now.Day)
+                cell.GetComponent<Image>().color = new Color(1f, 0.9f, 0.6f);
+        }
+    }
+}
