0644c7588273e92c3a11a43c6082d178da348fc1
nate254347 <nate254347@gmail.com>
2025-06-14 00:40:27 -0400

    Added timer
    functional Timer added to the clock scene

--- Assets/Scripts/Clock/Toggle.cs
+++ Assets/Scripts/Clock/Toggle.cs
@@ -5,9 +5,12 @@ public class ToggleButton : MonoBehaviour
 {
     public Transform targetObject;
     public Transform textObject;
+    public GameObject objectToToggle1;
+    public GameObject objectToToggle2;
 
     public void FlipX()
     {
+        
         if (targetObject != null)
         {
             Vector3 scale = targetObject.localScale;
@@ -20,6 +23,17 @@ public class ToggleButton : MonoBehaviour
             Vector3 scale = textObject.localScale;
             scale.x *= -1f;
             textObject.localScale = scale;
+        }
+
+        
+        if (objectToToggle1 != null)
+        {
+            objectToToggle1.SetActive(!objectToToggle1.activeSelf);
+        }
+
+        if (objectToToggle2 != null)
+        {
+            objectToToggle2.SetActive(!objectToToggle2.activeSelf);
         }
     }
 }
